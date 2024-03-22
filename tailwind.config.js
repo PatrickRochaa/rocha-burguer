3b6095f6680dead2df62d084408cd67af5004b14
Patrick Rocha <patrick.rochapereira@hotmail.com>
2024-03-22 20:25:11 -0300

    commit initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       backgroundImage: {
         home: "url('/assets/bg.png')",
       },
+      animation: {
+        fade: "fade 1s ease-in-out",
+      },
+      keyframes: {
+        fade: {
+          "0%": { transform: "translate3d(-30px, 0, 0)" },
+          "100%": { transform: "translate3d(0px, 0, 0)" },
+        },
+      },
     },
   },
   plugins: [],
