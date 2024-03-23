721f928de41612eed524aae415349d0fb5f7ac4c
Patrick Rocha <patrick.rochapereira@hotmail.com>
2024-03-22 21:46:50 -0300

    commit

--- modules/horario.js
+++ modules/horario.js
@@ -3,13 +3,20 @@ checkRestaurantOpen();
 
 export default function initHorario() {
   const spanItem = document.getElementById("date-span");
+  const footer = document.getElementById("footer");
   const isOpen = checkRestaurantOpen();
 
   if (isOpen) {
     spanItem.classList.remove("bg-red-500");
+    footer.classList.remove("bg-red-500");
+
+    footer.classList.add("bg-green-600");
     spanItem.classList.add("bg-green-600");
   } else {
     spanItem.classList.remove("bg-green-600");
+    footer.classList.remove("bg-green-600");
+
+    footer.classList.add("bg-red-500");
     spanItem.classList.add("bg-red-500");
   }
 }
